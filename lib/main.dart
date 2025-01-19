1df91e49939ff7e20a0b0d44f41a317de8076d1d
manu-180 <manumanu97@hotmail.com>
2025-01-19 18:46:02 -0300

    Agrega comentario de prueba en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:taller_ceramica/config/theme/app_theme.dart';
 import 'package:taller_ceramica/l10n/app_localizations.dart'; 
 import 'package:taller_ceramica/providers/theme_provider.dart';
 import 'package:taller_ceramica/subscription/subscription_manager.dart';
-
+//probando
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
